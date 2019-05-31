d10685f2c88abfbf5a2446f0e0d1ebf347a4ed99
Seth Lachman <lachmanseth17@gmail.com>
2019-05-31 13:28:02 -0400

    Added axios and showing list of projects.

--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -1,10 +1,46 @@
 import React from "react";
+import axios from "axios";
 
 class ProjectList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      projects: [],
+      fetchingProjects: true,
+      error: ""
+    };
+  }
+
+  componentDidMount() {
+    return axios
+      .get("http://localhost:4000/api/projects/")
+      .then(res =>
+        this.setState({ fetchingProjects: false, projects: res.data.projects })
+      )
+      .catch(err => this.setState({ fetchingProjects: false, error: err }));
+  }
+
   render() {
+    if (this.state.fetchingProjects) {
+      return (
+        <div>
+          <h2>Fetching Projects</h2>
+        </div>
+      );
+    }
     return (
       <div>
-        <h2>List of Projects</h2>
+        <h2>Current Projects</h2>
+        {this.state.error && <p>{this.state.error}</p>}
+        {this.state.projects &&
+          this.state.projects.map(project => {
+            return (
+              <div key={project.id}>
+                <p>{project.name}</p>
+              </div>
+            );
+          })}
       </div>
     );
   }
